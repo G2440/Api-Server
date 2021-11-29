deaebc105f39a8adba6195f017d9678dabc94620
g2440 <gauravnegi2440@gmail.com>
2021-11-29 08:39:01 +0530

    Added System Panel

--- boxapp/admin.py
+++ boxapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Profile,Store,Box
+
+admin.site.register(Profile)
+admin.site.register(Store)
+admin.site.register(Box)
